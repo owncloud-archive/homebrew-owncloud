8bb5e8439aee9b94e0469522c848ea6213828384
Alejandro Exojo <suy@badopi.org>
2015-08-30 13:26:06 +0200

    Change the Qt dependencies to optional
    According to the FAQ, there is no way to pass arguments to a formula,
    and the user has to run `brew edit`. We at least mitigate it by making
    Qt optional, so users can skip installing Qt if they have a copy
    already.
    
    https://github.com/Homebrew/homebrew/blob/7e45de0509487ec9280abc950a0369fc447385b8/share/doc/homebrew/FAQ.md#how-can-i-specify-different-configure-arguments-for-a-formula

--- client.rb
+++ client.rb
@@ -5,14 +5,17 @@ class Client < Formula
   head 'https://github.com/owncloud/client.git'
   depends_on 'cmake' => :build
   depends_on 'qtkeychain'
-  depends_on 'qt5'
+  # Depends on Qt, but we want to accept a system Qt as well. According to the
+  # FAQ, there is no way to configure a formula with a parameter (as of August
+  # 2015), and the only way is to let the user run `brew edit` after failure.
+  depends_on 'qt5' => :optional
 
   def install
     # Dependency tracking only, uncomment this section only if you know what you
     # are doing!
     # mkdir 'build'
     # cd 'build' do
-    #   system "cmake .. #{std_cmake_parameters}"
+    #   system "cmake", "..", "-DCMAKE_PREFIX_PATH=/usr/local/opt/qt5", *std_cmake_args
     #   system "make package"
     # end
   end
--- qtkeychain.rb
+++ qtkeychain.rb
@@ -9,7 +9,10 @@ class Qtkeychain < Formula
   sha1 '38a55bec4720107ec5da40fa785cb0ae5b8bdd6c'
 
   depends_on 'cmake' => :build
-  # depends on Qt, but we want to accept a system Qt as well. How?
+  # Depends on Qt, but we want to accept a system Qt as well. According to the
+  # FAQ, there is no way to configure a formula with a parameter (as of August
+  # 2015), and the only way is to let the user run `brew edit` after failure.
+  depends_on 'qt5' => :optional
 
   def install
     system "cmake", ".", "-DCMAKE_PREFIX_PATH=/usr/local/opt/qt5", *std_cmake_args

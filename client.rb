732e3cc916b61dc84819ea6215cb15c924663a57
Alejandro Exojo <suy@badopi.org>
2015-08-30 12:11:48 +0200

    Update the client formula
    - Add homepage and change the repository URL.
    - Drop ocsync and neon. Add qtkeychain.
    - Reindent the install section. Probably could be left uncommented, and
      have the documentation state that the preferred way is to use `brew
      install --only-dependencies`.

--- client.rb
+++ client.rb
@@ -1,29 +1,26 @@
 require 'formula'
 
-class Mirall < Formula
-  homepage ''
-  head 'git://gitorious.org/owncloud/mirall.git'
+class Client < Formula
+  homepage 'https://github.com/owncloud/client'
+  head 'https://github.com/owncloud/client.git'
   depends_on 'cmake' => :build
-# ocsync should not be installed
-#  depends_on 'ocsync'
+  depends_on 'qtkeychain'
   depends_on 'qt5'
-  depends_on 'neon'
 
-def install
-#  Dependency tracking only, uncomment this section only if you know what you
-#  are doing!
-#
-#     mkdir 'build'
-#     cd 'build' do
-#      system "cmake .. #{std_cmake_parameters}"
-#      system "make package"
-#     end
-end
+  def install
+    # Dependency tracking only, uncomment this section only if you know what you
+    # are doing!
+    # mkdir 'build'
+    # cd 'build' do
+    #   system "cmake .. #{std_cmake_parameters}"
+    #   system "make package"
+    # end
+  end
 
   def test
     # This test will fail and we won't accept that! It's enough to just replace
     # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test csync`.
+    # were more thorough. Run the test with `brew test client`.
     system "false"
   end
 end

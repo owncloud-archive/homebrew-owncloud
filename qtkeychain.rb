a37691690e66de90e83842251d3a953024bd1244
Daniel Molkentin <danimo@owncloud.com>
2016-05-09 14:49:47 +0200

    Drop Qt4 support again, doesn't seem to work

--- qtkeychain.rb
+++ qtkeychain.rb
@@ -8,20 +8,12 @@ class Qtkeychain < Formula
   url 'https://github.com/frankosterfeld/qtkeychain/archive/v0.6.2.tar.gz'
   sha256 'ae13459234feeeab3a154457319d9b26ee9600973443517c77e055838ebae63c'
 
-  option "without-qt4", "Build against Qt5 instead of Qt4 (default)"
-
   depends_on 'cmake' => :build
-  depends_on "qt" if build.with? "qt4"
-  depends_on "qt5" if build.without? "qt4"
+  depends_on "qt5"
 
   def install
-    if build.with? "qt4"
-      system "cmake", ".", "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_prefix}", *std_cmake_args
-    else
-      system "cmake", ".", "-DCMAKE_PREFIX_PATH=#{Formula["qt5"].opt_prefix}", *std_cmake_args
-
-    system "make install" # if this fails, try separate make/make install steps
-    end
+    system "cmake", ".", "-DCMAKE_PREFIX_PATH=#{Formula["qt5"].opt_prefix}", *std_cmake_args
+    system "make install"
   end
 
   def test

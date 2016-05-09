bf6eadc81797124e405ec02c278e8691f7620a1c
Daniel Molkentin <danimo@owncloud.com>
2016-05-09 14:32:26 +0200

    try to properly detect qt, also support qt4

--- qtkeychain.rb
+++ qtkeychain.rb
@@ -8,12 +8,20 @@ class Qtkeychain < Formula
   url 'https://github.com/frankosterfeld/qtkeychain/archive/v0.6.2.tar.gz'
   sha256 'ae13459234feeeab3a154457319d9b26ee9600973443517c77e055838ebae63c'
 
+  option "without-qt4", "Build against Qt5 instead of Qt4 (default)"
+
   depends_on 'cmake' => :build
-  # depends on Qt, but we want to accept a system Qt as well. How?
+  depends_on "qt" if build.with? "qt4"
+  depends_on "qt5" if build.without? "qt4"
 
   def install
-    system "cmake", ".", "-DCMAKE_PREFIX_PATH=/usr/local/opt/qt5", *std_cmake_args
+    if build.with? "qt4"
+      system "cmake", ".", "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_prefix}", *std_cmake_args
+    else
+      system "cmake", ".", "-DCMAKE_PREFIX_PATH=#{Formula["qt5"].opt_prefix}", *std_cmake_args
+
     system "make install" # if this fails, try separate make/make install steps
+    end
   end
 
   def test
